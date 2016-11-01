98b687b14979ea31df40655db3354405ba4786c2
Matthias Hirzel <hirzel@hermine.(none)>
2016-11-01 23:08:47 +0100

    JSON-Parser added

--- JsonParser/json/JsonToTreeParserTest.java
+++ JsonParser/json/JsonToTreeParserTest.java
@@ -56,6 +56,58 @@ public class JsonToTreeParserTest {
 		assertEquals(expected, actual);
 	}
 	
+	@Test
+	public void testParse2() {
+		// {"data":[{"type":"articles","id":"1","attributes":{"title":"JSON API paints my bikeshed!","body":"The shortest article. Ever.","created":"2015-05-22T14:56:29.000Z","updated":"2015-05-22T14:56:28.000Z"},"relationships":{"author":{"data":{"id":"42","type":"people"}}}}],"included":[{"type":"people","id":"42","attributes":{"name":"John","age":80,"gender":"male"}}]}
+
+		String json = "{" +
+		   "\"data\":[" +
+		      "{" +
+		         "\"type\":\"articles\"," +
+		         "\"id\":\"1\"," +
+		         "\"attributes\":{" +
+		            "\"title\":\"JSON API paints my bikeshed!\"," +
+		            "\"body\":\"The shortest article. Ever.\"," +
+		            "\"created\":\"2015-05-22T14:56:29.000Z\"," +
+		            "\"updated\":\"2015-05-22T14:56:28.000Z\"" +
+		         "}," +
+		         "\"relationships\":{" +
+		            "\"author\":{" +
+		               "\"data\":{" +
+		                  "\"id\":\"42\"," +
+		                  "\"type\":\"people\"" +
+		               "}" +
+		            "}" +
+		         "}" +
+		      "}" +
+		   "]," +
+		   "\"included\":[" +
+		      "{" +
+		         "\"type\":\"people\"," +
+		         "\"id\":\"42\"," +
+		         "\"attributes\":{" +
+		            "\"name\":\"John\"," +
+		            "\"age\":80," +
+		            "\"gender\":\"male\"" +
+		         "}" +
+		      "}," +
+		      "{" +
+		         "\"type\":\"people\"," +
+		         "\"id\":\"42\"," +
+		         "\"attributes\":{" +
+		            "\"name\":\"John\"," +
+		            "\"age\":80," +
+		            "\"gender\":\"male\"" +
+		         "}" +
+		      "}" +
+		   "]" +
+		"}";
+		final Node tree = jsonToTreeParser.parse(json);
+		final String actual = jsonToTreeParser.traverseNodes(tree);
+		final String expected = "+Label: root+---Label: data+------Label: data_1|      - Key: id|        Value: 1|      - Key: type|        Value: articles+---------Label: attributes|         - Key: body|           Value: The shortest article. Ever.|         - Key: title|           Value: JSON API paints my bikeshed!|         - Key: updated|           Value: 2015-05-22T14:56:28.000Z|         - Key: created|           Value: 2015-05-22T14:56:29.000Z+---------Label: relationships+------------Label: author+---------------Label: data|               - Key: id|                 Value: 42|               - Key: type|                 Value: people+---Label: included+------Label: included_1|      - Key: id|        Value: 42|      - Key: type|        Value: people+---------Label: attributes|         - Key: age|           Value: 80|         - Key: name|           Value: John|         - Key: gender|           Value: male+------Label: included_2|      - Key: id|        Value: 42|      - Key: type|        Value: people+---------Label: attributes|         - Key: age|           Value: 80|         - Key: name|           Value: John|         - Key: gender|           Value: male";
+		assertEquals(expected, actual);
+	}
+	
 //	@Test
 //	public void testParse2() {
 //		// {"glossary": {"title": "example glossary","GlossDiv": {"title": "S","GlossList": {"GlossEntry": {"ID": "SGML", "SortAs": "SGML", "GlossTerm": "Standard Generalized Markup Language", "Acronym": "SGML", "Abbrev": "ISO 8879:1986", "GlossDef": {"para": "A meta-markup language, used to create markup languages such as DocBook.", "GlossSeeAlso": ["GML", "XML"]}, "GlossSee": "markup"}}, "a2": "b2"}}, "abc": "x", "def": "y"}
